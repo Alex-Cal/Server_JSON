8dd7299e852943a62383b05fd3bd0b1058614344
alex <alessandro.muciaccia1996@gmail.com>
2021-06-24 13:47:52 +0200

    First

--- Server.py
+++ Server.py
@@ -1,16 +1,19 @@
 from bottle import run, request, post, get
-import json
+import pymongo
+from bson import json_util
 
 
 @get('/')
 def index():
-    element = []
-    with open('/home/alex/IdeaProjects/Server_JSON/Eventi.json', 'r') as f:
-        file = json.load(f)
-    #for value in file:
-        #if value['Role'] == 'Editor':
-        #element.append(json.dumps(value))
-    return json.dumps(file)
+    # Connection to MongoDB
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["CalendarDB"]
+    Cal = mydb["Calendar"]
+    Event = mydb["Events"]
+    items = Cal.find({}, {'_id': 0, 'Events': 1})
+    res = items[0]['Events']
+    items_ev = Event.find({'_id': res}, {'Title': 1, 'Start': 1, 'End': 1})
+    return json_util.dumps(items_ev)
 
 
 run(host='0.0.0.0', port=12345, debug=True)
