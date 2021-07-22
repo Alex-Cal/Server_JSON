166e16b2967e7e2c426c04d604c2cc86c9f957c3
Alex-Cal <83657658+Alex-Cal@users.noreply.github.com>
2021-07-22 15:50:55 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,6 +4,7 @@ import re
 from datetime import datetime
 
 from bottle import run, request, post, get, app, response, route
+from bottle_cors_plugin import cors_plugin
 import pymongo
 import bottle
 from bson import json_util, ObjectId
@@ -29,8 +30,7 @@ class EnableCors(object):
             # set CORS headers
             response.headers['Access-Control-Allow-Origin'] = '*'
             response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
-            response.headers[
-                'Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
             if bottle.request.method != 'OPTIONS':
                 # actual request; reply with the actual response
